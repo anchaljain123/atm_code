cbc9edda8fa1d8e104ef32fd76459ac9e8c59833
anchaljain123 <jain.anchal1493@gmail.com>
2017-03-13 12:11:58 +0530

    changes in check()

--- project/new.js
+++ project/new.js
@@ -4,24 +4,23 @@ $(document).ready(function(){
     /*
      Class Atm for assigning notes value to array & printing Table
      */
-    var   result=0;
+    var   result=0,bank_bal=0;
     var leftamount =0;
 
-    function Atm()
-    {
-        this.WithdrawalAmount=0;
-        this.note_array=[2000,500,100,50];
-        var len=this.note_array.length;
-        this.count_array=[];
+    function Atm() {
 
-        for (var i =0; i<len; i++) {// assigning  0 to count_array elements
-            this.count_array[i] = 0;
+        note_array = [2000, 500, 100, 50];
+        len = note_array.length;
+        count_array = [];
+
+        for (var i = 0; i < len; i++) {// assigning  0 to count_array elements
+            count_array[i] = 0;
         }
 
         /**
          * This function is use to take inputs from Banker side
          */
-        this.fillATM = function() {
+        this.fillATM = function () {
             var value = $("#max_limit").val(); //Assigning Max Limit for a day in the value
 
             if (value % 50 != 0 || value == 0 || value < 0) { //Enter only in Multiples of 50 notes,not negative nor 0
@@ -31,27 +30,27 @@ $(document).ready(function(){
                 if (($("#2000").val() != null) && ($("#500").val() != null) && ($("#100").val() != null) && ($("#50").val() != null )
                     && ($("#max_limit").val() != null )) //adding freq of notes for 1st time only
                 {
-                    this.count_array[0] = $('#2000').val();
-                    this.count_array[1] = $('#500').val();
-                    this.count_array[2] = $('#100').val();
-                    this.count_array[3] = $('#50').val();
+                    count_array[0] = $('#2000').val();
+                    count_array[1] = $('#500').val();
+                    count_array[2] = $('#100').val();
+                    count_array[3] = $('#50').val();
 
-                    for(var j=0;j<len;j++){//calculating leftamount in bank
-                        leftamount+=this.count_array[j]*this.note_array[j];
+                    for (var j = 0; j < len; j++) {//calculating leftamount in bank
+                        leftamount +=count_array[j] *note_array[j];
                     }
 
-                    result=leftamount;
+                    result = leftamount;
 
                     var str = "<tr id='fillatm'>" + //update table only for the first time
                         "<td>" + result + "<\/td>" +
-                        "<td>" + this.count_array[0] + "<\/td>" +
-                        "<td>" + this.count_array[1] + "<\/td>" +
-                        "<td>" + this.count_array[2] + "<\/td>" +
-                        "<td>" + this.count_array[3] + "<\/td>" +
+                        "<td>" + count_array[0] + "<\/td>" +
+                        "<td>" + count_array[1] + "<\/td>" +
+                        "<td>" + count_array[2] + "<\/td>" +
+                        "<td>" + count_array[3] + "<\/td>" +
                         "<td>" + result + "<\/td>" +
                         "</tr>";
                     $('#table1').find('tbody').append(str);
-                    $('#currentAmount').html(this.leftAmount);
+                    $('#currentAmount').html(leftamount);
                     $('#fillatm').css({'background-color': 'green'});
                     $("#btn1").attr("disabled", true);
                 }
@@ -61,33 +60,108 @@ $(document).ready(function(){
 
             }
         }
+
+    }
+    /*
+     class to withdraw on customer requirement
+     */
+
+    function Log(){
+
+        this.withdrawAmount = $("#amount").val();
+        this.custAmount = parseInt(this.withdrawAmount);
+        this.notereq = [];
+
+        for (var i = 0; i < len; i++) {// assigning  0 to count_array elements
+            this.notereq[i] = 0;
+        }
+        var k=0;
+
+        var temp=this.custAmount;
+        console.log(this.notereq);
+
+        for (var j = 0; j < len; j++) {// calculating notes req by customer
+            this.notereq[j] =Math.floor(this.custAmount / note_array[j]);
+            /*if(this.notereq[j]<=count_array[j])*/
+            this.custAmount%=note_array[j];
+
+        }
+        this.custAmount=temp;
+        bank_bal=leftamount;
+
+        /*
+         function to withdraw amount from ATM
+         */
+
+        this.check=function() {
+
+            var t,diffamt;
+            while(k!=len) {//if bank has more money than required
+
+
+                if (this.notereq[k] <=count_array[k]) {
+
+
+                    if(this.notereq[k]-count_array[k]==0){
+                        diffamt=(note_array[k]*count_array[k]);//when equal
+
+
+                    }
+                    else
+                    {
+                        diffamt=(note_array[k]*this.notereq[k]);//when notereq is less
+
+
+                    }
+
+                    this.custAmount-=diffamt;
+                    bank_bal-=diffamt;
+
+                    count_array[k] -= this.notereq[k];//from log table
+
+
+                }
+
+                else{
+
+                    this.notereq[k]-=count_array[k];
+                    count_array[k]=0;
+                    t=Math.floor((this.notereq[k]*note_array[k])/note_array[k+1]);//making notes
+                    this.notereq[k]=0;
+                    this.notereq[k+1]+=t;
+                }
+
+                k++;
+                console.log(bank_bal + " " + k + " " + leftamount);
+                leftamount=bank_bal
+
+            }
+
+
+        }
         /*
          Function to update Log Table when withdraw amount
          */
-        this.addLog=function ()
-        {
+
+        this.addLog = function () {
             var str = "<tr>" +
-                "<td>" + leftamount +"<\/td>" +
-                "<td>" + this.count_array[0] +"<\/td>" +
-                "<td>" + this.count_array[1] +"<\/td>" +
-                "<td>" + this.count_array[2] +"<\/td>" +
-                "<td>" +this.count_array[3] +"<\/td>" +
-                "<td>" +result +"<\/td>" +
+                "<td>" + this.withdrawAmount + "<\/td>" +
+                "<td>" + count_array[0] + "<\/td>" +
+                "<td>" + count_array[1] + "<\/td>" +
+                "<td>" + count_array[2] + "<\/td>" +
+                "<td>" + count_array[3] + "<\/td>" +
+                "<td>" + leftamount + "<\/td>" +
                 "</tr>";
             $('#table1').find('tbody').append(str);
-            $('#currentAmount').html(o.leftAmount);
-            $('#table1 tbody tr').css({ 'background-color' : 'red'});
-            $('#fillatm').css({ 'background-color' : 'green'});
+
+            $('#currentAmount').html(leftamount);
+
+            $('#table1 tbody tr').css({'background-color': 'red'});
+            $('#fillatm').css({'background-color': 'green'});
         }
 
+
     }
-
-
-    /*
-     function to withdraw on customer requirement
-     */
-
-
 
 
 
@@ -96,17 +170,14 @@ $(document).ready(function(){
         var object_atm=new Atm();
         object_atm.fillATM()
 
-    })
+    });
 
+    $('#btn2').on("click",function () {
 
-
-
-
-
-
-
-
-
+        var cust_obj=new Log();//for each customer
+        cust_obj.check();
+        cust_obj.addLog();
+    });
 
 
 
